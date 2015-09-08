b93e690d1e9621bdcf7100f348900edaf5cc6919
Bryan Co <bryan.lao.co@gmail.com>
2015-09-07 23:18:53 -0700

    Added search and Comment controller.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ActiveRecord::Base
     validates :title, presence: true, uniqueness: true
+
+    def self.search(value)
+      search_term = "%#{value}%"
+      where(["title ILIKE :term OR body ILIKE :term", {term: search_term}])
+    end
+
 end
